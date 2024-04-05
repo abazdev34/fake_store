b513a5415fb1416100a0fc424c674dfd3571fc9f
abazdev34 <abaztoktorbaev89@gmail.com>
2024-04-05 10:25:48 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { type } from '@testing-library/user-event/dist/type';
+import './App.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Basket from './pages/Basket';
+import Liket from './pages/Liket';
+import Slayder from './pages/Slayder';
+import CardSk from './pages/CardSk';
+
+
+function App() {
+
+
+
+  return (
+    <div>
+     
+    
+
+
+
+      <Header />
+      <Routes>
+        
+        <Route path="/Home" element={<Home />} />
+        <Route path="/basket" element={<Basket />} />
+        <Route path="/liket" element={<Liket />} />
+       </Routes>
+    </div>
+  );
+}
+
+export default App;
